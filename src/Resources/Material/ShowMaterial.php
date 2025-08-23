a92038d118538db7e6d0871974ce8bc991c14b27
Hamzah <hamzahnafalah@gmail.com>
2025-08-23 14:58:58 +0700

    update modules

--- src/Resources/Material/ShowMaterial.php
+++ src/Resources/Material/ShowMaterial.php
@@ -7,17 +7,9 @@ class ShowMaterial extends ViewMaterial
     public function toArray(\Illuminate\Http\Request $request): array
     {
         $arr = [
-<<<<<<< HEAD
             'item' => $this->relationValidation('item',function(){
                 return $this->item->toViewApi()->resolve();
             })
-=======
-            'bill_of_materials' => $this->relationValidation('billOfMaterials',function(){
-                return $this->billOfMaterials->transform(function($billOfMaterial){
-                   return $billOfMaterial->toViewApi(); 
-                });
-            })  
->>>>>>> 6bb9ba27f10bc3c2cf16c263baa8023b121eec43
         ];
         $arr  = array_merge(parent::toArray($request), $arr);
         return $arr;
